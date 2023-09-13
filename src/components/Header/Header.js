79fac37a5d78df9ba1c6a0e086fc86cd050be887
erShiaVa <vakili.ershia@gmail.com>
2023-09-13 14:06:41 +0200

    feature__navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,22 +12,22 @@ function Header() {
   }
 
   return (
-      <div className={`{toggle ? 'expanded' : 'nav'} nav container`}>
+      <div className={`nav container ${toggle ? 'expanded' : ''}`}>
         <div className="nav__logo">
             <img src={assets.logo_nav} alt='#' />
         </div>
         
-        <ul className="nav__items">
+        <ul className={`nav__items ${toggle ? 'show' : ''}`}>
             <li>Home</li>
             <li>Services</li>
             <li>Testimonials</li>
             <li>News</li>
+            <button className="button-secondary">Get in Touch</button>
         </ul>
         <div className="toggle-icon" onClick={handleToggle}>
           {toggle ? <CgClose size={22}/> : <CgMenuRight size={22}/>}
         </div>
       </div>
-    
   )
 }
 
