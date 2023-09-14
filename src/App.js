584e29dacf85b20c9846eb1e896d70607738cb4b
erShiaVa <vakili.ershia@gmail.com>
2023-09-14 15:50:08 +0200

    feature__video

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Boost from './components/Boost/Boost';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import ServiceSection from './components/ServiceSection/ServiceSection';
+import Video from './components/VideoSection/Video';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Hero/>
       <ServiceSection/>
       <Boost/>
+      <Video/>
     </div>
   );
 }
--- src/assets/index.js
+++ src/assets/index.js
@@ -7,9 +7,11 @@ export const assets = {
     partner_two: require("../assets/partner-1-2.png"),
     partner_three: require("../assets/partner-1-3.png"),
     boost_img: require("../assets/boost-1-1.png"),
+    video__img: require("../assets/video.png"),
     service_one: require("./icons/service-1-1.svg").default,
     service_two: require("./icons/service-1-2.svg").default,
     service_three: require("./icons/service-1-3.svg").default,
     service_four: require("./icons/service-1-4.svg").default,
     check_mark: require("./icons/check-mark.svg").default,
+    arrow: require("./icons/comment-1.svg").default,
 };
